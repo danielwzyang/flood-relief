0fbcb59fb14ca9cdbe904627b58843718fc2cbb8
Daniel Yang <danielwzyang@gmail.com>
2024-09-29 01:31:32 -0400

    updated team cards

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import "./globals.css"
 import background from "../assets/bg.jpg"
 import Image from "next/image"
 import TeamCard from "@/components/ui/teamcard"
+import placeholder from "../assets/profile.png"
+import daniel from "../assets/daniel.png"
 
 export default function Home() {
     return (
@@ -11,23 +13,29 @@ export default function Home() {
             <div className="h-[60px] w-full flex items-center justify-center ">
                 <h1 className="text-xl sm:text-2xl">Flood Relief Network</h1>
             </div>
-            <Image src={background} alt="background" className="w-full max-h-[750px] object-cover object-center" />
-            <div className="my-10 flex flex-wrap w-fit justify-center mx-auto">
+            <div className="w-full h-screen max-h-[750px]">
+                <Image src={background} alt="background" loading={"lazy"} className="w-full h-full object-cover object-center" />
+            </div>
+
+            <h1 className="text-center mt-5 text-2xl ">Our Team</h1>
+            <div className="my-5 flex flex-wrap w-fit justify-center mx-auto gap-5">
                 <TeamCard
                     name="Junghoon Yu"
                     position="president"
                     bio="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste corrupti fugit sit dolores aperiam doloribus explicabo laborum cupiditate sequi perspiciatis?"
-                    facebook=":("
+                    facebook="Junghoon Yu"
                     instagram="yungyooon"
-                    mail=":("
+                    mail="jhoony9960@gmail.com"
+                    image={placeholder}
                 />
                 <TeamCard
                     name="Seoyeon Yu"
                     position="president"
                     bio="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste corrupti fugit sit dolores aperiam doloribus explicabo laborum cupiditate sequi perspiciatis?"
-                    facebook=":("
+                    facebook="Seoyeon Yu"
                     instagram="shkoyeon"
-                    mail=":("
+                    mail="seoyeonyu0412@gmail.com"
+                    image={placeholder}
                 />
                 <TeamCard
                     name="Thomas Choi"
@@ -36,19 +44,18 @@ export default function Home() {
                     facebook=":("
                     instagram="thomas._.cho1"
                     mail=":("
+                    image={placeholder}
                 />
                 <TeamCard
                     name="Daniel Yang"
                     position="it director"
-                    bio="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste corrupti fugit sit dolores aperiam doloribus explicabo laborum cupiditate sequi perspiciatis?"
-                    facebook="@danielwzyang"
+                    bio="My hobbies are photography and programming. I like to collect figurines, and my favorite movie is Good Will Hunting."
+                    facebook="Daniel Yang"
                     instagram="@danielwzyang"
                     mail="danielwzyang@gmail.com"
+                    image={daniel}
                 />
             </div>
-            <p className="m-auto my-10 w-[1200px] max-w-[85vw] text-justify">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perferendis eveniet eius voluptatem vitae possimus ducimus amet quae commodi delectus, placeat veritatis deserunt quibusdam dignissimos nihil debitis enim. Voluptate ipsam odit voluptates rem a iusto ullam exercitationem repellendus doloremque. Eos expedita rerum omnis cumque aliquid magnam deleniti unde! Doloremque consectetur, eveniet alias deserunt repudiandae inventore impedit unde voluptatibus corrupti, culpa excepturi optio enim fuga magnam amet voluptate vel. Labore ea, totam quaerat debitis error adipisci blanditiis eaque consectetur nihil placeat voluptates, pariatur impedit? Voluptatibus sit libero iste harum, recusandae nulla maxime, dolorem alias expedita cumque quibusdam ratione. Excepturi veritatis ducimus cum quasi amet exercitationem aperiam sed minima doloremque omnis cupiditate error, ea nostrum, nemo, necessitatibus consequuntur iure dignissimos quae assumenda illum distinctio ipsa atque accusantium voluptatum? At id tempora, impedit suscipit excepturi ullam earum provident repudiandae alias. Corrupti et, quas quos nisi molestiae veritatis recusandae, reiciendis possimus, consequatur perspiciatis nam? Hic numquam officia provident molestiae error earum id voluptates, accusamus nemo? Iste vel reiciendis tempora. Repellendus atque aut placeat quas hic ducimus, voluptatum neque perspiciatis maiores quod blanditiis doloremque, delectus corrupti, enim vitae. Vel magnam voluptas id, commodi porro rem modi quos impedit reiciendis et, esse recusandae ex ipsam voluptatem dolorum voluptates suscipit accusamus dolorem, eius fuga sapiente. Iste perferendis laboriosam at eos, ipsam porro unde fugit quibusdam eligendi distinctio animi sequi ab explicabo soluta maiores harum facilis voluptatem dolores dolore amet dolorum adipisci officia rerum consectetur. Distinctio, dolorem asperiores et eum officia repudiandae magnam, necessitatibus quis molestias saepe dicta neque, omnis nulla nobis accusantium molestiae ipsa sapiente laboriosam incidunt eaque! Corporis facere natus tempora tenetur voluptate cum perspiciatis. Aut repudiandae reprehenderit, quae temporibus dolore iusto esse tenetur quaerat, est, eos ipsam assumenda consequuntur cupiditate iure? Provident veritatis cumque nesciunt? Laudantium amet accusamus facere nam reprehenderit. Pariatur repellat similique quis quo fugiat asperiores, commodi labore repudiandae animi esse, laborum, quidem exercitationem nam error laboriosam obcaecati sed quod! A qui fugit sunt, asperiores repellat omnis molestiae sit deleniti, in et voluptatibus earum iure voluptate est assumenda vitae amet consequatur beatae quis. Obcaecati, explicabo suscipit expedita id ex adipisci ab eligendi soluta incidunt tempore dolor ratione optio assumenda? Soluta, beatae quia eum aperiam consectetur quas quos laudantium quis non neque, omnis nemo quam minus nobis. Culpa, numquam possimus sapiente inventore earum iste autem. Illum doloremque ut laudantium pariatur, esse obcaecati temporibus labore, animi commodi adipisci, veritatis illo repudiandae ducimus error eligendi velit corrupti. Voluptas error accusamus adipisci aut ex, eius doloribus aliquid similique eos commodi quod cumque aspernatur, ipsam accusantium culpa cupiditate autem ipsum voluptatem ratione. Accusamus nulla eaque consequuntur earum minus culpa ipsum omnis iure vitae, atque voluptate, alias similique quam voluptates, necessitatibus cum. Quos repellendus quam atque assumenda porro ex?
-            </p>
         </div>
     )
 }
--- components/ui/teamcard.tsx
+++ components/ui/teamcard.tsx
@@ -7,8 +7,7 @@ import {
     CardTitle,
 } from "./card"
 
-import Image from "next/image"
-import profile from "../../assets/profile.png"
+import Image, { StaticImageData } from "next/image"
 import facebook from "../../assets/facebook.svg"
 import instagram from "../../assets/instagram.svg"
 import mail from "../../assets/mail.svg"
@@ -21,33 +20,34 @@ export interface cardProps {
     bio: string,
     facebook: string,
     instagram: string,
-    mail: string
+    mail: string,
+    image: StaticImageData
 }
 
 export default function TeamCard(props: cardProps) {
     return (
-        <Card className="w-[300px] m-5 border-[#b6b6b6]">
+        <Card className="w-[300px] h-[350px] border-[#b6b6b6] relative">
             <CardHeader className="relative">
-                <Image src={profile} alt="picture" width={80} height={80} className="absolute right-5 top-5 rounded-xl" />
+                <Image src={props.image} alt="picture" width={80} height={80} className="absolute right-5 top-5 rounded-xl" />
                 <CardTitle className="text-lg">{props.name}</CardTitle>
                 <CardDescription className="text-md">{props.position}</CardDescription>
             </CardHeader>
             <CardContent className="mt-6 text-balance">{props.bio}</CardContent>
-            <CardFooter>
-                <div className="flex h-5 items-center space-x-3 ml-auto mr-2">
+            <CardFooter className="absolute bottom-0 right-0">
+                <div className="flex h-5 items-center space-x-3">
                     <Popover>
                         <PopoverTrigger><Image src={facebook} alt="facebook" /></PopoverTrigger>
-                        <PopoverContent className="w-fit py-2 rounded-xl text-sm">{props.facebook}</PopoverContent>
+                        <PopoverContent className="w-fit py-2 rounded-xl text-sm" side="top">{props.facebook}</PopoverContent>
                     </Popover>
                     <Separator orientation="vertical" className="bg-[#b6b6b6]" />
                     <Popover>
                         <PopoverTrigger><Image src={instagram} alt="instagram" /></PopoverTrigger>
-                        <PopoverContent className="w-fit py-2 rounded-xl text-sm">{props.instagram}</PopoverContent>
+                        <PopoverContent className="w-fit py-2 rounded-xl text-sm" side="top">{props.instagram}</PopoverContent>
                     </Popover>
                     <Separator orientation="vertical" className="bg-[#b6b6b6]" />
                     <Popover>
                         <PopoverTrigger><Image src={mail} alt="mail" /></PopoverTrigger>
-                        <PopoverContent className="w-fit py-2 rounded-xl text-sm">{props.mail}</PopoverContent>
+                        <PopoverContent className="w-fit py-2 rounded-xl text-sm" side="top">{props.mail}</PopoverContent>
                     </Popover>
                 </div>
             </CardFooter>
