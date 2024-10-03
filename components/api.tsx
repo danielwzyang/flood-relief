c7ee3b794ea2cea217a160ddb0bef6dc09bcda2c
Daniel Yang <danielwzyang@gmail.com>
2024-10-02 22:20:18 -0400

    fixing no live update for calendar api by removing cache

--- components/api.tsx
+++ components/api.tsx
@@ -2,7 +2,11 @@ import axios from "axios"
 
 export async function getEvents() {
     try {
-        const response = await axios.get(`https://www.googleapis.com/calendar/v3/calendars/${process.env.CALENDAR}/events?key=${process.env.API_KEY}`)
+        const response = await axios.get(`https://www.googleapis.com/calendar/v3/calendars/${process.env.CALENDAR}/events?key=${process.env.API_KEY}`, {
+            headers: {
+                "Cache-Control": "no-cache"
+            }
+        })
         return response.data.items
     } catch (error) {
         console.error("error fetching events", error)
